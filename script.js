b4a34e6f11dca7ef25d2b4ca40851b193ebedb2e
kuykear <kuykear@gmail.com>
2022-05-11 23:29:31 -0500

    Finished "Round" and "Game" Functions

--- script.js
+++ script.js
@@ -4,63 +4,76 @@
 //  -scissors
 //RETURN: Computers selection
 
-let winner;
-let playerSelection;
+let playerSelection = "paper";
 let computerSelection;
+let computerScore = 0;
+let playerScore = 0;
 
 function computerPlay(){
-    const randNum = Math.round(Math.random()*2);
+    const randNum = Math.floor(Math.random()*3);
     switch(randNum){
         case 0:
-            computerSelection = "Rock"
-            break;
+            return "Rock";
         case 1:
-            computerSelection = "Paper"
-            break;
+            return "Paper";
         case 2:
-            computerSelection = "Scissors"
-            break;
+            return "Scissors";
         default:
-            computerSelection = "Error calculating answer"
+            return "Error calculating answer";
     }
 }
 
 //Create a function called 'playRound' that will play 1 round and pulls the following parameters:
 //  - playerSelection
-//  - computerSelection
 //RETURN: string that declares who won this round
 
-function playRound(playerSelection, computerSelection){
-
-    
-    playerSelection.toLowerCase();
-    computerSelection.toLowerCase();
-
-    if (playerSelection == "rock" && computerSelection == "scissors"){
-        winner = "Player wins!"
-    } else if (playerSelection == "rock" && computerSelection == "paper"){
-        winner = "Computer wins!"
-    } else if (playerSelection == "paper" && computerSelection == "scissors"){
-        winner = "Computer wins!"
-    } else if (playerSelection == "paper" && computerSelection == "rock"){
-        winner = "Player wins!"
-    } else if (playerSelection == "scissors" && computerSelection == "rock"){
-        winner = "Computer wins!"
-    } else if (playerSelection == "scissors" && computerSelection == "paper"){
-        winner = "Player wins!"
-    } else{
-        winner = "Draw!"
-    }
-
-
-}
+function playRound(playerSelection){
+    //Convert to strings for comparison
+    const computerSelection = String(computerPlay()).toLowerCase();
+    let playerSelectionStringLower = String(playerSelection).toLowerCase();
+    console.log(computerSelection);
+    console.log(playerSelectionStringLower)
+    if (computerSelection == playerSelectionStringLower){
+                playerScore++;
+                computerScore++;
+                return "Tie Game!";
+        } else if (
+            computerSelection == "rock" && playerSelectionStringLower == "scissors" ||
+            computerSelection == "scissors" && playerSelectionStringLower == "paper" ||
+            computerSelection == "paper" && playerSelectionStringLower == "rock"
+            ){
+                computerScore++;
+                return "Computer wins!";
+            } else {
+                playerScore++;
+                return "Player win";
+            }
+    };
 
 //Create a function called 'game' that will call the playRound function and will
 //  - play 5 rounds
 //for (let i = 0; i < 5; i++) {
     // your code here!
-    console.log(winner);
+    //console.log(winner);
  //}
 //  - keep score for all 5 rounds
 //  - declare the winner for each round
 //RETURN: declare the winner or loser at the end
+
+function game(){
+    for (let i = 0; i<5; i++){
+        winner = playRound(playerSelection);
+        console.log(winner);
+        console.log(playerScore);
+        console.log(computerScore);
+    }
+    switch(true){
+        case playerScore > computerScore:
+            return "Player wins the match!";
+        case computerScore > playerScore:
+            return "Computer wins the match!";
+        default:
+            return "We have a tie!"
+    }
+
+}
